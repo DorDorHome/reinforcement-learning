7756198aa040b6b1d1eaacf4b2678163b3096b16
SHU FAI CHAN <shufaichan@SHUs-MacBook-Air.local>
2024-05-15 21:40:49 +0800

    updated sweeping

---
+++ Dyna_planning/prioritized_sweeping_deterministic.py
@@ -0,0 +1,24 @@
+# implementing the prioritized sweeping for deterministic environment
+# refer to chapter 8, 8.4
+
+#  tasks: see the best way to implement a priority queue
+
+
+def prioritized_sweeping_deter_env():
+    """
+    input:
+
+    
+    
+    
+    
+    
+    """
+
+
+    # initialize a Q function 
+
+    # initialize a Queue
+
+
+
